0cdc633c1ad34bee51f7ef61b44ddbb99b110cb6
Kajetan Orzełek <kajetan.orzelek@protonmail.com>
2022-03-31 11:30:30 +0200

    Nuitka fiexes

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ import scrapy
 import ArtveeScraper
 import ArtveeScraper.pipelines
 import ArtveeScraper.items
+import scrapy.spiderloader
+import scrapy.statscollectors
 from src.Ui import Ui
 
 if __name__ == "__main__":
