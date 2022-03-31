6cf07abeda065f1931c2973fa216632c2a482976
Kajetan Orzełek <kajetan.orzelek@protonmail.com>
2022-03-31 12:49:06 +0200

    Cleaunup

--- main.py
+++ main.py
@@ -1,12 +1,4 @@
-import os
-import sys
 import app
-import scrapy
-import ArtveeScraper
-import ArtveeScraper.pipelines
-import ArtveeScraper.items
-import scrapy.spiderloader
-import scrapy.statscollectors
 from src.Ui import Ui
 
 if __name__ == "__main__":
